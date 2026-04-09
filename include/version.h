3fbeaf7884f5be9b2f429d5ce7683837b902b282
petrosij <skdudarnovpetr11@gmail.com>
2026-04-10 01:25:45 +0300

    feat: final test

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 // Автоматически сгенерировано, не редактировать вручную
-#define PROJECT_VERSION "1.3.3"
+#define PROJECT_VERSION "1.4.0"
 #define PROJECT_VERSION_MAJOR 
 #define PROJECT_VERSION_MINOR 
 #define PROJECT_VERSION_PATCH 
